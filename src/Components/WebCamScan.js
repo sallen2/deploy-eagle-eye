6b18750a1b3e7fba4ddbde09ed015248d310ad62
Stephan Allen <sallen202009@gmail.com>
2019-02-07 18:16:35 -0500

    rekognition creds

--- src/Components/WebCamScan.js
+++ src/Components/WebCamScan.js
@@ -4,7 +4,13 @@ import Webcam from 'react-webcam'
 import Promise from 'bluebird'
 import Button from '@material-ui/core/Button';
 
-const rek = new AWS.Rekognition({region: 'us-east-1'})
+const rek = new AWS.Rekognition({
+  credentials: {
+    secretAccessKey: process.env.SECRET_ACCESS_KEY,
+    accessKeyId: process.env.ACCESS_KEY
+  },
+  region: 'us-east-1'
+})
 const s3 = new AWS.S3({region: 'us-east-1'})
 
 const lambda = new AWS.Lambda()
